ab6620fa7e4c53d4a4389f3d4410193deb1494e4
Rustam Gurd <rustamkurdov@mail.ru>
2019-03-22 11:00:04 +0300

    Studied how to work with object initialization of component

--- src/main/java/com/lab/controller/MainPage.java
+++ src/main/java/com/lab/controller/MainPage.java
@@ -1,5 +1,6 @@
 package com.lab.controller;
 
+import com.lab.model.Operand;
 import com.lab.service.Calculation;
 import com.lab.service.Operation;
 import org.apache.wicket.markup.html.WebPage;
@@ -8,8 +9,9 @@ import org.apache.wicket.markup.html.form.Button;
 import org.apache.wicket.markup.html.form.DropDownChoice;
 import org.apache.wicket.markup.html.form.Form;
 import org.apache.wicket.markup.html.form.TextField;
-import org.apache.wicket.model.AbstractReadOnlyModel;
+import org.apache.wicket.model.IModel;
 import org.apache.wicket.model.Model;
+import org.apache.wicket.model.PropertyModel;
 
 import java.util.Arrays;
 
@@ -27,47 +29,54 @@ public class MainPage extends WebPage {
         private DropDownChoice<Operation> operations;
         private Button submit;
 
-        private Label result;
+        private Label resultField;
+
+        private Operand operandObject1;
+        private Operand operandObject2;
+        private Operand resultObject;
 
         // TODO: 21.03.2019 Extract AbstractReadOnlyModel for less code
         public CalculationForm(String id) {
             super(id);
 
-            add(new Label("label_operands", new AbstractReadOnlyModel<String>() {
-                @Override
-                public String getObject() {
-                    return getString("label_operands");
-                }
-            }));
-            add(this.operandField1 = new TextField<>("operandField1", new Model<>()));
-            add(this.operandField2 = new TextField<>("operandField2", new Model<>()));
+            operandObject1 = new Operand();
+            operandObject2 = new Operand();
+            resultObject = new Operand();
 
-            add(new Label("label_operations", new AbstractReadOnlyModel<String>() {
-                @Override
-                public String getObject() {
-                    return getString("label_operations");
-                }
-            }));
+            add(new Label("label_operands", (IModel) () -> getString("label_operands")));
+            add(this.operandField1 = new TextField<>("operandField1", new PropertyModel(operandObject1, "value")));
+            add(this.operandField2 = new TextField<>("operandField2", new PropertyModel(operandObject2, "value")));
+
+            add(new Label("label_operations", (IModel) () -> getString("label_operations")));
+
+            // TODO: Normal initialization with creation of basic model
             add(this.operations = new DropDownChoice<>("operations", new Model<>(), Arrays.asList(Operation.values())));
             add(this.submit = new Button("submit"));
 
-            add(new Label("label_result", new AbstractReadOnlyModel<String>() {
-                @Override
-                public String getObject() {
-                    return getString("label_result");
-                }
-            }));
-            add(this.result = new Label("lbl_4", new Model<>("")));
+            // TODO: This is the normal Label initialization for java 1.7
+//            add(new Label("label_result", new AbstractReadOnlyModel<String>() {
+//                @Override
+//                public String getObject() {
+//                    return getString("label_result");
+//                }
+//            }));
+
+            // TODO: This is how it looks in java 1.8 (lambda)
+            add(new Label("label_result", (IModel) () -> getString("label_result")));
+
+            add(this.resultField = new Label("lbl_4", new PropertyModel(resultObject, "value")));
+
         }
 
         @Override
         public void onSubmit() {
-            System.out.println("I'm done");
-
             String operand1 = operandField1.getInput();
             String operand2 = operandField2.getInput();
             String calculation = Calculation.calculate(operations.getConvertedInput(), operand1, operand2).get(0);
-            result.setDefaultModel(new Model<>(calculation));
+            resultField.setDefaultModel(new Model<>(calculation));
+
+            // TODO: Here we check if value really have changed
+            System.out.println(operandObject1.getValue());
         }
     }
 }
---
+++ src/main/java/com/lab/model/Operand.java
@@ -0,0 +1,13 @@
+package com.lab.model;
+
+public class Operand {
+    private String value = "0";
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
